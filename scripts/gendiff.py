b8a474664ac61698e458c3b43a30e2611749c1c8
Юлия Варфоломеева <uliagorbenko73@gmail.com>
2025-05-03 16:02:07 +0400

    new directory

---
+++ scripts/gendiff.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Gendiff: Show difference between two files.')
+    # Добавьте сюда описание других аргументов, если нужно
+    args = parser.parse_args()
+    # Ваша основная логика здесь
+
+if __name__ == '__main__':
+    main()
