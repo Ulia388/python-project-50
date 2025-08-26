4dfef3da99f46ecef1b5cc7652f8feb5652171e5
Юлия Варфоломеева <uliagorbenko73@gmail.com>
2025-08-26 14:52:09 +0400

    remove

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,14 +1,16 @@
+from gendiff.generate_diff import generate_diff
 import unittest
 import tempfile
 import os
 import json
 
-from gendiff.generate_diff import generate_diff
 
 class TestGenerateDiff(unittest.TestCase):
 
     def create_temp_file(self, content):
-        tmp = tempfile.NamedTemporaryFile(mode='w+', delete=False)
+        tmp = tempfile.NamedTemporaryFile(
+            mode='w+', delete=False
+        )
         json.dump(content, tmp)
         tmp.close()
         return tmp.name
@@ -30,11 +32,16 @@ class TestGenerateDiff(unittest.TestCase):
             c: true
         }'''
         result = generate_diff(self.f1, self.f2)
-        self.assertEqual(self.normalize_output(result), self.normalize_output(expected))
+        self.assertEqual(
+            self.normalize_output(result),
+            self.normalize_output(expected)
+        )
 
     def normalize_output(self, output):
         
-        return "\n".join(line.strip() for line in output.strip().splitlines())   
+        return "\n".join(
+            line.strip() for line in output.strip().splitlines()
+        )   
 
     def test_add_key(self):
         data1 = {"a": 1}
@@ -46,7 +53,10 @@ class TestGenerateDiff(unittest.TestCase):
         + b: 2
         }'''
         result = generate_diff(self.f1, self.f2)
-        self.assertEqual(self.normalize_output(result), self.normalize_output(expected))
+        self.assertEqual(
+            self.normalize_output(result),
+            self.normalize_output(expected)
+        )
 
     def test_remove_key(self):
         data1 = {"a": 1, "b": 2}
@@ -58,7 +68,10 @@ class TestGenerateDiff(unittest.TestCase):
         - b: 2
         }'''
         result = generate_diff(self.f1, self.f2)
-        self.assertEqual(self.normalize_output(result), self.normalize_output(expected))
+        self.assertEqual(
+            self.normalize_output(result),
+            self.normalize_output(expected)
+        )
 
     def test_change_key_value(self):
         data1 = {"a": 1, "b": 2}
@@ -71,7 +84,11 @@ class TestGenerateDiff(unittest.TestCase):
         + b: 3
         }'''
         result = generate_diff(self.f1, self.f2)
-        self.assertEqual(self.normalize_output(result), self.normalize_output(expected))
+        self.assertEqual(
+            self.normalize_output(result),
+            self.normalize_output(expected)
+        )
 
-if __name__ == '__main__':
-    unittest.main()
+
+    if __name__ == '__main__':
+        unittest.main()
